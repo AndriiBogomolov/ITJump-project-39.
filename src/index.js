2a51d9aeb0c546e0aa6e41a455d6d97136d26f0b
Iryna Kolomoiec <kolomoyka@gmail.com>
2022-08-29 21:59:19 +0200

    add modal effects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 (() => {
   const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+ 
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+(() => {
+  const refs = {
 
     headerOpenModalBtn: document.querySelector(".page-header__button[data-modal-open]"),
     heroOpenModalBtn: document.querySelector(".hero__button[data-modal-open]"),
@@ -19,3 +33,5 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+
